40e368f26686a18dcb3d3c23072c023b06484d50
plutoese <glen.zhang7@gmail.com>
2023-10-23 18:53:17 +0800

    cloudstudio prisma version 0.02

---
+++ cloudstudio/version231023/app/api/user/createuser/route.js
@@ -0,0 +1,29 @@
+import prisma from "@/lib/db/prisma";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(request) {
+  try {
+    const body = await request.json();
+    const { name, email, password } = body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const group = body.group ? body.group : "user";
+    const remark = body.remark ? body.remark : "";
+
+    const newUser = await prisma.User.create({
+      data: {
+        email: email,
+        name: name,
+        password: hashedPassword,
+        group: group,
+        isActive: true,
+        remark: remark,
+      },
+    });
+
+    return NextResponse.json(newUser);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "POST Error", error });
+  }
+}
